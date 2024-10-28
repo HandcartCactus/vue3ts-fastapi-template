2ea410924602dd46fe5104f740fa3192bddea8dd
Elias Jaffe <elijaffe173@gmail.com>
2024-10-28 12:37:56 -0400

    make app.vue just a router view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,37 +1,34 @@
 <template>
   <div>
-    <LoginComponent v-if="!authenticated" @authenticated="onAuthenticated" />
-    <div v-else>
+    <RouterView />
+    <!-- <div>
       <h2>Welcome to the App!</h2>
       <button @click="logout">Logout</button>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import LoginComponent from '@/components/LoginComponent.vue';
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    LoginComponent,
-  },
+  components: {},
   setup() {
-    const authenticated = ref(false);
+    const authenticated = ref(false)
 
     const onAuthenticated = (status: boolean) => {
-      authenticated.value = status;
-    };
+      authenticated.value = status
+    }
 
     const logout = () => {
-      localStorage.removeItem('accessToken');
-      authenticated.value = false;
-    };
+      localStorage.removeItem('accessToken')
+      authenticated.value = false
+    }
 
-    return { authenticated, onAuthenticated, logout };
+    return { authenticated, onAuthenticated, logout }
   },
-});
+})
 </script>
 
 <style scoped>
